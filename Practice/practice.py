e99471cea100c5c389317a744550cee45942cb37
SangWon Lee <97885055+SW0929@users.noreply.github.com>
2024-03-13 18:06:50 +0900

    2024/03/13

--- Practice/practice.py
+++ Practice/practice.py
@@ -233,3 +233,232 @@ age = 20
 color = "노란"
 print(f"나는 {age}살이며, {color}색을 좋아해요.")
 """
+
+#탈출 문자
+"""
+# \n : 줄바꿈
+print("백문이 불여일견\n 백견이 불여일타")
+# \" \' : 문장 내 "" or '' 출력
+print("저는 \"나도코딩\" 입니다.")
+# \\ : 문장 내에서 \
+print("C:\\coding\\Python\\Python_Algorithm>")
+# \r : 커서를 맨 앞으로 이동해줌
+print("Red Apple\rPine") # PineApple
+# \b : 백스페이스 (한 글자 삭제)
+print("redd\bApple") # redApple
+# \t : 탭
+print("Red\tApple")
+"""
+
+# Quiz 3) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오.
+"""
+예) http://naver.com
+규칙1 : http:// 부분은 제외 => naver.com
+규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 내 'e' 갯수 + "!" 로 구성
+                (naver)             (5)         (1)         (!)
+예) 생성된 비밀번호 : nav51!
+
+정답 )
+url = "http://youtube.com"
+my_str = url.replace("http://","") # 규칙1
+my_str = my_str[:my_str.index(".")] # 규칙2
+password =  my_str[:3] + str(len(my_str)) +str(my_str.count('e')) + '!' # 규칙3
+
+print("{0} 의 비밀번호는 {1} 입니다.".format(url, password))
+"""
+
+# 리스트 []
+"""
+subway = [10, 20, 30]
+print(subway)
+
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+
+# 조세호씨가 몇 번째 칸에 타고 있는가?
+print(subway.index("조세호"))
+
+# 하하씨가 다음 정류장에서 다음 칸에 탐
+subway.append("하하") # 리스트 멘 뒤 값 추가
+print(subway)
+
+# 정형돈씨를 유재석 / 조세호 사이에 태워봄
+subway.insert(1, "정형돈") # 원하는 인덱스에 값 삽입
+print(subway)
+
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print(subway)
+print(subway.count("유재석"))
+
+# 정렬도 가능
+num_list = [5,2,4,3,1]
+num_list.sort()
+print(num_list)
+
+# 순서 뒤집기 가능
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기
+num_list.clear()
+print(num_list)
+
+# 다양한 자료형 함께 사용
+mix_list = ["조세호", 20, True]
+print(mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list) # 리스트를 하나로 합침
+print(num_list)
+"""
+
+# 사전 {키:값}
+"""
+cabinet = {3:"유재석", 100:"김태호"}
+print(cabinet[3]) # 유재석
+print(cabinet[100]) # 김태호
+
+print(cabinet.get(3)) # 유재석
+# print(cabinet[5]) # Key Error!
+print(cabinet.get(5)) # None 출력
+print(cabinet.get(5, "사용 가능")) # default 값 출력
+
+print(3 in cabinet) # True/False 출력
+
+cabinet2 = {"A-3":"유재석", "B-100":"김태호"}
+print(cabinet2.get("A-3"))
+print(cabinet2.get("B-100"))
+
+# 새 손님
+cabinet2["A-3"] = "김종국"
+cabinet2["C-20"] = "조세호" # 만약 키 값이 있을 경우 값 수정 됨
+print(cabinet2)
+
+# 간 손님
+del cabinet2["A-3"] # 키 삭제
+print(cabinet2)
+
+# key 들만 출력
+print(cabinet2.keys())
+# value 들만 출력
+print(cabinet2.values())
+
+# key, value 쌍으로 출력
+print(cabinet2.items())
+
+# 목욕탕 폐점
+cabinet2.clear()
+print(cabinet2)
+"""
+
+# 튜플
+# - 내용 변경 및 추가 불가
+# - 속도가 리스트 보다 빠름
+# - 변경되지 않는 블록 사용시 유리
+"""
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+name = "김종국"
+age = 20
+hobby = "운동"
+print(name, age, hobby)
+
+(name, age, hobby) = ("유재석", 50, "토크")
+print(name, age, hobby)
+"""
+
+# 집합 (set) {}
+# 중복 안됨, 순서 없음
+"""
+my_set = {1,2,3,3,3}
+print(my_set)
+
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+# 교집합 (java 와 python 을 모두 할 수 있는 사람)
+print(java & python)
+print(java.intersection(python))
+
+# 합집합 (java 도 할 수 있거나 python 할 수 있는 사람)
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java 는 할 수 있지만 python 은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python 할 줄 아는 사람이 늘어남
+python.add("김태호")
+print(python)
+
+# java 를 잊어버림
+java.remove("김태호")
+print(java)
+"""
+
+# 자료구조의 변경
+"""
+# 커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
+"""
+
+# Quiz 4)
+"""
+당신의 학교에서는 파이썬 코딩 대회를 주최합니다.
+참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다.
+댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+추첨 프로그램을 작성하시오.
+
+조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20 이라고 가정
+조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
+조건3 : random 모듈의 shuffle 과 sample 을 활용
+
+(출력 예제)
+ -- 담청자 발표 --
+ 치킨 당첨자 : 1
+ 커피 당첨자 : [2, 3, 4]
+  -- 축하합니다 --
+
+(활용 예제)
+from random import *
+lst = [1, 2, 3, 4, 5]
+print(lst)
+shuffle(lst)
+print(lst)
+print(sample(lst,1)) # 첫 번째 인자에서 두 번째 인자만큼 뽑겠다.
+
+정답)
+from random import *
+lst = list(range(1, 21)) # 1부터 20 까지 숫자를 생성
+shuffle(lst)
+winners = sample(lst, 4)
+print(" -- 당첨자 발표 --")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print(" -- 축하합니다 --")
+"""
