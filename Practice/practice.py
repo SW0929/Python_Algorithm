285fee24a263aeab91c2bfb2ce3b1a3374e41019
SangWon Lee <97885055+SW0929@users.noreply.github.com>
2024-03-23 09:54:11 +0900

    2024/03/23_01

--- Practice/practice.py
+++ Practice/practice.py
@@ -574,7 +574,9 @@ print(students)
 [ ] 50번째 손님 (소요시간 : 16분)
 
 총 탑승 승객 : 2 분
-"""
+
+# 정답 )
+
 from random import *
 total_customer = 0 # 총 탑승 승객
 for i in range(1, 51):
@@ -586,3 +588,179 @@ for i in range(1, 51):
         print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, customer))
 
 print("총 탑승 승객 : {0} 분".format(total_customer))
+"""
+
+# ----- 함수 -----
+'''
+"""
+def 함수명():
+   수행내용
+"""
+def open_account(): # 함수 생성
+    print("새로운 계좌가 생성되었습니다.")
+open_account() # 함수 호출
+
+# ----- 전달값과 반환값 -----
+def deposit(balance, money):
+    print("입급이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance + money))
+    return balance + money
+
+def withdraw(balance, money): # 출금
+    if balance >= money: #잔액이 출금보다 많으면
+        print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
+        return balance - money
+    else:
+        print("출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.".format(balance))
+        return balance
+    
+def withdraw_night(balance, money): # 저녁에 출금
+    commission = 100 # 수수료 100원
+    return commission, balance - money - commission
+
+balance = 0 # 잔액
+balance = deposit(balance, 1000)
+balance = withdraw(balance, 500)
+commission, balance = withdraw_night(balance, 500)
+print("수수료 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
+
+# ----- 전달값과 반환값 -----
+
+# ----- 기본값 -----
+def profile(name, age, main_lang):
+    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}" 
+          .format(name, age, main_lang))
+profile("유재석", 20, "파이썬")
+profile("김태호", 25, "자바")    
+
+# 같은 학교 같은 학년 같은 반 같은 수업
+
+def profile(name, age=17, main_lang="파이썬"):
+    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}" 
+          .format(name, age, main_lang))
+    
+profile("유재석")
+profile("김태호")
+# ----- 기본값 -----
+
+# ----- 키워드 값 -----
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name="유재석", main_lang="파이썬", age=20)
+profile(main_lang="자바", age=25, name="김태호")
+# ----- 키워드 값 -----
+
+# ----- 가변인자 -----
+# 기본 함수
+def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+    print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+    # 문장 마지막에 end=" " 을 적으면 다음 코드를 이어서 출력하게 한다.
+    print(lang1, lang2, lang3, lang4, lang5)
+
+# 가변 인자를 사용한 함수
+def profile(name, age, *language): # 서로 다른 갯수의 값을 넣을 때 *매개변수 사용
+    print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+    # 문장 마지막에 end=" " 을 적으면 다음 코드를 이어서 출력하게 한다.
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+
+profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+profile("김태호", 25, "kotlin", "Swift")
+# ----- 가변인자 -----
+
+# ----- 지역변수와 전역변수 -----
+"""
+지역변수 : 함수가 호출될 때 만들어지며 함수가 끝나면 사라짐.
+전역변수 : 프로그램 내 모든 공간에서 호출 가능한 변수.
+※ 일반적으로 전역변수를 많이 사용하지 않음.(코드 관리가 어려워짐.)
+"""
+gun = 10
+
+def checkpoint(soldiers): # 경계근무
+    global gun # 전역 공간에 있는 gun 사용
+    # gun = 20 지역 변수
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+
+def checkpoint_ret(gun, soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+
+print("전체 총 : {0}".format(gun))
+checkpoint(2) # 2명이 경계 근무 나감
+gun = checkpoint_ret(gun , 2)
+print("남은 총 : {0}". format(gun))
+# ----- 지역변수와 전역변수 -----
+'''
+# ----- 함수 -----
+
+# Quiz 6) 표준 체중을 구하는 프로그램을 작성하시오.
+"""
+* 표준 체중 : 각 개인의 키에 적당한 체중
+
+(성별에 따른 공식)
+남자 : 키(m) X 키(m) X 22
+여자 : 키(m) X 키(m) X 21
+
+조건1 : 표준 체중은 별도의 함수 내에서 계산
+        * 함수명 : std_weight
+        * 전달값 : 키(height), 성별(gender)
+조건2 : 표준 체중은 소수점 둘째자리까지 표시
+
+(출력 예제)
+키 175cm 남자의 표준 체중은 67.38kg 입니다.
+
+
+# 정답 1)
+def std_weight(height, gender):
+    if gender == "남자":
+        average_weight = height * height * 22 # 남자
+    else:
+        average_weight = height * height * 21 # 여자
+
+    print("키 {0}cm {1}의 표준 체중은 {2:.2f}kg 입니다.".format(height, gender, average_weight))
+    # {2:.2f} 는 콜론 앞 숫자는 format index, 뒤 숫자는 소숫점 2까지 출력
+    # 즉, 소숫점 3번째 자리를 반올림 하여 출력한다.
+
+std_weight(175/100, "남자")
+
+# 정답 2) 이게 더 맞음
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22 # 남자
+    else:
+        return height * height * 21 # 여자
+
+height = 175 # cm 단위
+gender = "남자"
+weight = round(std_weight(height/100, gender), 2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
+"""
+
+# 표준 입출력
+"""
+print("Python", "Java", sep=",") # Python,Java 즉 콤마 부분에 구분자 입력
+print("무엇이 더 재밌을까요", end="?") # 뒷 문장을 붙여서 출력
+import sys
+print("Python", "Java", file=sys.stdout) # 표준 출력으로 문장이 찍힘
+print("Python", "Java", file=sys.stderr) # 표준 에러로 출력
+
+# 시험 성적
+scores = {"수학":0, "영어":50, "코딩":100}
+for subhect, score in scores.items(): # 키, 값을 쌍을 보내줌
+    #print(subhect, score)
+    print(subhect.ljust(8), str(score).rjust(4), sep=":")
+    # ljust(숫자) : 숫자만큼의 공간을 확보하고 왼쪽 정렬
+    # rjust(숫자) : 숫자만큼의 공간을 확보하고 오른쪽 정렬
+
+# 은행 대기순번표
+# 001, 002, 003, ...
+for num in range(1, 21):
+    print("대기번호 : " + str(num).zfill(3))
+    # zfill(숫자) : 숫자 만큼의 공간을 확보하고 값이 없는 부분은 0을 채움
+
+answer = input("아무 값이나 입력하세요 : ") # 기본 타입은 string
+"""
