5538c87bb1df7d66dd1b202016a4568bc1a67c56
SangWon Lee <97885055+SW0929@users.noreply.github.com>
2025-03-29 21:59:36 +0900

    .

--- baekjoon/.cph/CodingTest/ch03_2304&14719.py
+++ baekjoon/.cph/CodingTest/ch03_2304&14719.py
@@ -1,2 +1,22 @@
 # 2304 & 14719
 # 완전 탐색 | 누적합 | 투포인터 로 풀 수 있음
+
+N = int(input())
+warehouse = [list(map(int, input().split())) for _ in range(N)]
+
+
+warehouse.sort(key=lambda x:x[1])
+big_col = warehouse[-1][1] # 기둥 높이가 가장 큰 값
+big_row = warehouse[-1][0] # 기둥 위치
+
+print(big_row, big_col)
+warehouse.sort()
+
+brick = 0
+ 
+height = warehouse[0][0] # 초기 값 위치
+for i in range(height, big_row):
+    
+
+
+print(brick)
