e8efd529b2407138b4e79ce4f1a14a46537c2c8a
sw <sjugas1234@naver.com>
2025-04-29 13:46:57 +0900

    2025/04/29

---
+++ baekjoon/Softeer/[21년 재직자 대회 예선] 전광판.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+infor = {
+    '0': '1111110',
+    '1': '0110000',
+    '2': '1101101',
+    '3': '1111001',
+    '4': '0110011',
+    '5': '1011011',
+    '6': '1011111',
+    '7': '1110010',
+    '8': '1111111',
+    '9': '1111011',
+    ' ': '0000000'
+}
+
+
+for _ in range(T):
+    a, b = map(str, input().rstrip().split())
+    
+    a = a.rjust(5)
+    b = b.rjust(5)
+    
+    result = 0
+    for i in range(5):
+        for j in range(7):
+            if (infor[a[i]][j] != infor[b[i]][j]):
+                result += 1
+
+    
+    print(result)
+    
